ce16dac66844539f4705597f53175bfaf954aba0
Hari Krishna Goli <harikrishna.goli@hotmail.com>
2025-11-20 01:21:03 +0530

    Version 1.0 with sqlalchemy

--- app/models.py
+++ app/models.py
@@ -1,17 +1,19 @@
 # Pydantic models (request/response schemas)
 from pydantic import BaseModel
+from typing import Optional
+from datetime import datetime
 
 
 # Request model (input JSON for /shorten)
 class URLRequest(BaseModel):
-    owner_id: str
+    owner_id: str | None = None
     long_url: str
     custom_alias: str | None = None
-    expires_at: str | None = None
+    expires_at: Optional[datetime] = None
 
 # Response model (output JSON for /shorten)
 class URLResponse(BaseModel):
     short_url: str
     owner_id: str
-    expires_at: str | None = None
+    expires_at: Optional[datetime] = None
     message: str
